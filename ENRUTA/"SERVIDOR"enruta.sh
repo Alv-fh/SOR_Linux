c65e8d85e2cce47a09768728bd4645c05d2cedda
Álvaro <109484163+Alv-fh@users.noreply.github.com>
2024-02-11 16:42:13 +0100

    Update "SERVIDOR"enruta.sh

--- ENRUTA/"SERVIDOR"enruta.sh
+++ ENRUTA/"SERVIDOR"enruta.sh
@@ -6,5 +6,25 @@ echo "1" > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A MASQUERADE -s 193.168.3.0 -o ens18 -j MASQUERADE
 iptables -A FORWARD -j ACCEPT
 
+#Se le da permisos
+chmod +x enruta.sh
+#Se ejecuta
+bash enruta.sh
+#En el (CLIENTE) se hace ping y debe de funcionar
+ping 8.8.8.8
+
+#Una vez comprobado, lo vamos a hacer servicio
+
+sudo apt-get update; sudo apt-get upgrade -y
+sudo apt-get install iptables-persistent #(SERVIDOR)
+
+
+
+
+
+
+
+
+
 
 
